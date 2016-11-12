65a9cc4a83ce721d087230a3f4839d064aebef63
Patryk Grudniewski <patgrudniewski@gmail.com>
2016-11-12 14:16:29 +0100

    empty main script

---
+++ main.cpp
@@ -0,0 +1,13 @@
+/**
+ * @author Patryk Grudniewski <patgrudniewski@gmail.com>
+ */
+
+/**
+ * @param int argc
+ * @param char* argv[]
+ * @return int
+ */
+int main(int argc, char* argv[])
+{
+    return 0;
+}
