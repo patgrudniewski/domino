23ca483a56dfbc4ff0131c66d702483cdfa25656
Patryk Grudniewski <patgrudniewski@gmail.com>
2016-11-19 22:55:48 +0100

    board position not empty exception methods

--- src/Domino/Exception/BoardPositionNotEmptyException.cpp
+++ src/Domino/Exception/BoardPositionNotEmptyException.cpp
@@ -6,8 +6,30 @@
 namespace Domino::Exception {
     using namespace std;
 
-    BoardPositionNotEmptyException::BoardPositionNotEmptyException()
+    /**
+     * @param int x
+     * @param int y
+     */
+    BoardPositionNotEmptyException::BoardPositionNotEmptyException(int x, int y)
         :runtime_error("Board position is not empty")
     {
+        this->position.first = x;
+        this->position.second = y;
+    }
+
+    /**
+     * @return int
+     */
+    int BoardPositionNotEmptyException::getPositionX()
+    {
+        return this->position.first;
+    }
+
+    /**
+     * @return int
+     */
+    int BoardPositionNotEmptyException::getPositionY()
+    {
+        return this->position.second;
     }
 }
--- src/Domino/Exception/BoardPositionNotEmptyException.h
+++ src/Domino/Exception/BoardPositionNotEmptyException.h
@@ -1,16 +1,34 @@
 /**
  * @author Patryk Grudniewski <patgrudniewski@gmail.com>
  */
-#ifndef EXCEPTION_BOARDPOSITIONNOTEMPTYEXCEPTION_H
-#define EXCEPTION_BOARDPOSITIONNOTEMPTYEXCEPTION_H
+#ifndef DOMINO_EXCEPTION_BOARDPOSITIONNOTEMPTYEXCEPTION_H
+#define DOMINO_EXCEPTION_BOARDPOSITIONNOTEMPTYEXCEPTION_H
 
 #include <stdexcept>
+#include <utility>
 
 namespace Domino::Exception {
     class BoardPositionNotEmptyException : public std::runtime_error
     {
         public:
-            BoardPositionNotEmptyException();
+            /**
+             * @param int x
+             * @param int y
+             */
+            BoardPositionNotEmptyException(int x, int y);
+
+            /**
+             * @return int
+             */
+            int getPositionX();
+
+            /**
+             * @return int
+             */
+            int getPositionY();
+
+        private:
+            std::pair<int, int> position;
     };
 }
 
